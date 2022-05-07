d4cc2fb42e4d8bb22b31f1acc60598e55533d3fb
raj3k <rajkosky@gmail.com>
2022-05-07 13:00:24 +0200

    Django view converted to Django Rest Framework view

--- api/views.py
+++ api/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from wods.models import Workout
 
 
+
+@api_view(["GET"])
 def index(request):
-    data = {"Hello": "World!"}
-    return JsonResponse(data)
+    model_data = Workout.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=["name", "mode"])
+    return Response(data)
