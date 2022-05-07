72560c959d4b06b20997f8f4e468fdac86e80c4e
raj3k <rajkosky@gmail.com>
2022-05-07 13:59:43 +0200

    api_home view completed for now

--- api/views.py
+++ api/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from wods.models import Workout
+from wods.serializers import WorkoutSerializer
 
 
 
 @api_view(["GET"])
-def index(request):
-    model_data = Workout.objects.all().order_by("?").first()
+def api_home(request):
+    instance = Workout.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=["name", "mode"])
+    if instance:
+        data = WorkoutSerializer(instance).data
     return Response(data)
