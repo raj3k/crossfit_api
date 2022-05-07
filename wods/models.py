057d42a6d3e49eb37730c6dd19cbe0a8552e8d7e
raj3k <rajkosky@gmail.com>
2022-05-07 12:25:36 +0200

    models created

--- wods/models.py
+++ wods/models.py
@@ -1,3 +1,29 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Exercise(models.Model):
+    name = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Workout(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=120)
+    mode = models.CharField(max_length=120)
+    equipment = models.ManyToManyField(Equipment)
+    exercises = models.ManyToManyField(Exercise)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    trainer_tips = models.TextField(blank=True, null=True)
+
