622d21cf3a7956dc13913992e56d8b7cf3e0dcc4
raj3k <rajkosky@gmail.com>
2022-05-10 19:30:35 +0200

    added serializers and overloaded create method in WorkoutSerializer

--- wods/serializers.py
+++ wods/serializers.py
@@ -2,19 +2,49 @@ from rest_framework import serializers
 from .models import Workout, Equipment, Exercise
 
 
-class WorkoutSerializer(serializers.ModelSerializer):
-    equipment = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field='name'
-    )
+class EquipmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Equipment
+        fields = ['name']
 
-    exercises = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field='name'
-    )
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = ['name']
+
+
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    # equipment = serializers.SlugRelatedField(
+    #     many=True,
+    #     read_only=True,
+    #     slug_field='name'
+    # )
+
+    # exercises = serializers.SlugRelatedField(
+    #     many=True,
+    #     read_only=True,
+    #     slug_field='name'
+    # )
+    equipment = EquipmentSerializer(many=True)
+    exercises = ExerciseSerializer(many=True)
+
 
     class Meta:
         model = Workout
         fields = ['id', 'name', 'mode', 'equipment', 'exercises', 'created_at', 'updated_at', 'trainer_tips']
+
+    def create(self, validated_data):
+        equipment_data = validated_data.pop('equipment')
+        exercises_data = validated_data.pop('exercises')
+        workout = Workout.objects.create(**validated_data)
+        for equipment in equipment_data:
+            workout.equipment.create(**equipment)
+
+        for exercise in exercises_data:
+            workout.exercises.create(**exercise)
+        return workout
+        
+    def update(self, instance, validated_data):
+        pass
