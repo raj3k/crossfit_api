27f23392241d15a29f36633146cebb74bee732df
raj3k <rajkosky@gmail.com>
2022-05-10 10:17:48 +0200

    views for listing, creating, retrieving, updateing and destroying

--- wods/views.py
+++ wods/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Workout, Equipment, Exercise
+from .serializers import WorkoutSerializer
+
+
+class WorkoutListAPIView(generics.ListCreateAPIView):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
+
+
+class WorkoutDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
+
+
