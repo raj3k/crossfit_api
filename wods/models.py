8a2b567d87542ee94ff1f357ed6b4f7fedb4d8ff
raj3k <rajkosky@gmail.com>
2022-05-13 13:18:12 +0200

    models rebuilt and initail migration

--- wods/models.py
+++ wods/models.py
@@ -2,27 +2,10 @@ import uuid
 from django.db import models
 
 
-class Exercise(models.Model):
-    name = models.TextField()
-
-    def __str__(self) -> str:
-        return self.name
-    
-
-
-class Equipment(models.Model):
-    name = models.CharField(max_length=120)
-
-    def __str__(self) -> str:
-        return self.name
-
-
 class Workout(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=120)
     mode = models.CharField(max_length=120)
-    equipment = models.ManyToManyField(Equipment)
-    exercises = models.ManyToManyField(Exercise)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     trainer_tips = models.TextField(blank=True, null=True)
@@ -31,3 +14,17 @@ class Workout(models.Model):
         return self.name
     
 
+class Exercise(models.Model):
+    content = models.TextField()
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.content
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=120)
+    workouts = models.ManyToManyField(Workout)
+
+    def __str__(self) -> str:
+        return self.name
