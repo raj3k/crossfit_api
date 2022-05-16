507a8c35668615aeadcf46319996442d654c4620
raj3k <rajkosky@gmail.com>
2022-05-16 18:32:12 +0200

    created endopints

--- wods/urls.py
+++ wods/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import WorkoutListAPIView, WorkoutDetailAPIView
+
 
 # /api/workouts/
 urlpatterns = [
