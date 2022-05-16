2783d3067badccde9a5a2961b80629bdff89437f
raj3k <rajkosky@gmail.com>
2022-05-16 23:11:13 +0200

    removed mixins from CBV

--- wods/views.py
+++ wods/views.py
@@ -4,37 +4,20 @@ from .models import Workout, Equipment, Exercise
 from .serializers import WorkoutSerializer
 
 
-class WorkoutListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class WorkoutListAPIView(generics.ListCreateAPIView):
     """
     List all Workout instances and create new Workout instance.
     """
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
-class WorkoutDetailAPIView(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class WorkoutDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieve, update and delete Workout instance.
+    Retrieve, update, partialy update and delete Workout instance.
     """
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
     lookup_field = 'pk'
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
 
