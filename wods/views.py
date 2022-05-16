e83e71a55209cd60ec9423c6d64062a77c15f420
raj3k <rajkosky@gmail.com>
2022-05-16 18:30:31 +0200

    create APIViews for listing, retrieving, updating and deleting data

--- wods/views.py
+++ wods/views.py
@@ -1,16 +1,33 @@
 from rest_framework import generics
-
+from rest_framework import mixins
 from .models import Workout, Equipment, Exercise
 from .serializers import WorkoutSerializer
 
 
-class WorkoutListAPIView(generics.ListCreateAPIView):
+class WorkoutListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
 
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-class WorkoutDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+class WorkoutDetailAPIView(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
